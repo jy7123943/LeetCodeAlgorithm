844ade8abfdfa07393ccc1cc963e797534a0def9
juyeonH <32149561+jy7123943@users.noreply.github.com>
2021-05-10 18:12:09 +0900

    Time: 96 ms (14.18%), Space: 41 MB (84.55%) - LeetHub

---
+++ max-consecutive-ones/max-consecutive-ones.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let result = 0;
+    
+    nums.reduce((acc, currentNum) => {
+        let nextAcc = acc;
+        
+        if (currentNum === 1) {
+            nextAcc++;
+        }
+
+        if (result < nextAcc) {
+            result = nextAcc;
+        }
+        
+        return currentNum === 0 ? 0 : nextAcc;
+    }, 0);
+    
+    return result;
+};
