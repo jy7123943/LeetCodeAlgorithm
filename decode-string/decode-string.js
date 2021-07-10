e237ebbb21b7a2ae1412fabd5e07213fd67ce663
juyeonH <32149561+jy7123943@users.noreply.github.com>
2021-07-10 11:37:34 +0900

    Time: 128 ms (5.01%), Space: 38.4 MB (62.93%) - LeetHub

--- decode-string/decode-string.js
+++ decode-string/decode-string.js
@@ -3,15 +3,18 @@
  * @return {string}
  */
 var decodeString = function(s) {
-  const regexp = /([0-9]+\[[a-z]+\])/;
+  const regexp = /([0-9]+\[[a-z]+\])/g;
   const patterns = s.match(regexp);
 
   if (patterns === null) return s;
 
-  const [pattern] = patterns;
-  const number = Number(pattern.match(/[0-9]+/)[0]);
-  const char = pattern.match(/[a-z]+/)[0];
-  const decoded = char.repeat(number);
+  const replacedStr = patterns.reduce((str, currentPattern) => {
+    const number = Number(currentPattern.match(/[0-9]+/)[0]);
+    const char = currentPattern.match(/[a-z]+/)[0];
+    const decoded = char.repeat(number);
 
-  return decodeString(s.replace(regexp, decoded));
+    return str.replace(currentPattern, decoded);
+  }, s);
+
+  return decodeString(replacedStr);
 };
