f318f47066af08fc047c61d94d44c8661b7f4525
Juyeon <jy7123943@gmail.com>
2021-07-18 17:24:46 +0900

    Time: 64 ms (96.71%), Space: 39.4 MB (43.19%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.js
@@ -2,24 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNonDuplicate = function(nums) {
-  const mid = Math.floor(nums.length / 2);
+var singleNonDuplicate = function(nums, left = 0, right = nums.length - 1) {
+  const mid = Math.floor((right - left) / 2) + left;
 
-  if (nums.length <= 3) {
-    if (nums[mid] === nums[mid + 1]) {
-      return nums[0];
-    } else {
-      return nums[nums.length - 1];
+  if (right - left <= 2) {
+    if (nums[left] === nums[left + 1]) {
+      return nums[right];
     }
+    return nums[left];
   }
 
   if (nums[mid] === nums[mid + 1]) {
-    return singleNonDuplicate(
-      mid % 2 === 0 ? nums.slice(mid) : nums.slice(0, mid)
-    );
-  } else {
-    return singleNonDuplicate(
-      mid % 2 === 0 ? nums.slice(0, mid + 1) : nums.slice(mid + 1)
-    );
+    if (mid % 2 === 0) {
+      return singleNonDuplicate(nums, mid, right);
+    }
+    return singleNonDuplicate(nums, 0, mid - 1);
   }
+
+  if (mid % 2 === 0) {
+    return singleNonDuplicate(nums, 0, mid);
+  }
+  return singleNonDuplicate(nums, mid + 1, right);
 };
