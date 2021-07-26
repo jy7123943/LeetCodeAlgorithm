e7ed0769e203b9823a32f6246c0ef52ca1e49c1d
Juyeon <jy7123943@gmail.com>
2021-07-26 12:36:16 +0900

    Add another solution

--- set-matrix-zeroes/set-matrix-zeroes.js
+++ set-matrix-zeroes/set-matrix-zeroes.js
@@ -27,3 +27,38 @@ var setZeroes = function(matrix) {
     });
   });
 };
+
+/**
+ * solution #2
+
+var setZeroes = function(matrix) {
+  let hasZeroInFirstRow = false;
+  let hasZeroInFirstCol = false;
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        if (i === 0) hasZeroInFirstRow = true;
+        if (j === 0) hasZeroInFirstCol = true;
+
+        matrix[i][0] = 0;
+        matrix[0][j] = 0;
+      }
+    }
+  }
+
+  for (let i = 1; i < matrix.length; i++) {
+    for (let j = 1; j < matrix[i].length; j++) {
+      if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+        matrix[i][j] = 0;
+      }
+    }
+    if (hasZeroInFirstCol) matrix[i][0] = 0;
+  }
+  if (hasZeroInFirstRow) {
+    matrix[0].fill(0);
+  } else if (hasZeroInFirstCol) {
+    matrix[0][0] = 0;
+  }
+};
+ */
