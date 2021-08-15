82b4e3369a1a3cc226ebcec7c07ee3dfa496d9c6
juyeonH <32149561+jy7123943@users.noreply.github.com>
2021-08-15 14:07:33 +0900

    Time: 92 ms (45.04%), Space: 48.2 MB (34.53%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let start = 0;
+  let end = height.length - 1;
+  let area = 0;
+
+  while (start <= end) {
+    const x = end - start;
+    const y = Math.min(height[start], height[end]);
+
+    if (height[start] < height[end]) {
+      area = Math.max(area, x * height[start]);
+      start++;
+    } else {
+      area = Math.max(area, x * height[end]);
+      end--;
+    }
+  }
+
+  return area;
+};
