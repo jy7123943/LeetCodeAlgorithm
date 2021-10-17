69de33afef04588af2a4196f2439dcbbe2d3a6dd
juyeonH <32149561+jy7123943@users.noreply.github.com>
2021-10-17 12:36:38 +0900

    Time: 72 ms (82.14%), Space: 39 MB (32.65%) - LeetHub

--- number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
+++ number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
@@ -4,12 +4,19 @@
  * @return {number}
  */
 var countStudents = function(students, sandwiches) {
-  while (students.includes(sandwiches[0])) {
+  let count = 0;
+
+  while (students.length) {
     if (sandwiches[0] === students[0]) {
       students.shift();
       sandwiches.shift();
+      count = 0;
     } else {
       students.push(students.shift());
+
+      if (count > students.length) break;
+
+      count++;
     }
   }
 
