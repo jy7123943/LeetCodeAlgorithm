644a620bfb262e4c5cc25b40089892a29e33d904
juyeonH <32149561+jy7123943@users.noreply.github.com>
2021-10-31 13:54:47 +0900

    Time: 80 ms (79.32%), Space: 43.3 MB (8.14%) - LeetHub

--- majority-element/majority-element.js
+++ majority-element/majority-element.js
@@ -3,16 +3,7 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-  let count = 0;
-  let candidate;
+  nums.sort((l, r) => l - r);
 
-  for (let i = 0; i < nums.length; i++) {
-    if (count === 0) {
-      candidate = nums[i];
-    }
-
-    count += candidate === nums[i] ? 1 : -1;
-  }
-
-  return candidate;
+  return nums[Math.floor(nums.length / 2)];
 };
