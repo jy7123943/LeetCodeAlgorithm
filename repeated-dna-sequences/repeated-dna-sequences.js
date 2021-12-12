e5809a4db4a4e7ca8f00f79eeb60ca5880417fc4
juyeonH <32149561+jy7123943@users.noreply.github.com>
2021-12-12 21:37:35 +0900

    Time: 120 ms (60.00%), Space: 60.6 MB (22.37%) - LeetHub

---
+++ repeated-dna-sequences/repeated-dna-sequences.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var findRepeatedDnaSequences = function(s) {
+  const result = [];
+  const hashtable = {};
+  for (let i = 0; i < s.length - 9; i++) {
+    const sequences = s.slice(i, i + 10);
+    if (!hashtable[sequences]) {
+      hashtable[sequences] = 1;
+      continue;
+    }
+
+    if (hashtable[sequences] === 1) {
+      hashtable[sequences]++;
+      result.push(sequences);
+    }
+  }
+
+  return result;
+};
