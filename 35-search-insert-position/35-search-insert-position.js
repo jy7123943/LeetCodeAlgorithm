608031839d9d65cffd716948bda0c5fead33e930
juyeonH <32149561+jy7123943@users.noreply.github.com>
2022-01-14 22:10:01 +0900

    Time: 68 ms (92.51%), Space: 40.2 MB (11.11%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,17 +4,14 @@
  * @return {number}
  */
 var searchInsert = function(nums, target, startIndex = 0) {
-  if (nums.length === 0) return startIndex;
-  if (nums.length === 1) {
-    if (nums[0] >= target) return startIndex;
+  if (nums.length <= 1) {
+    if (!nums[0] || nums[0] >= target) return startIndex;
     if (nums[0] < target) return startIndex + 1;
   }
 
   const midIndex = Math.floor(nums.length / 2);
 
-  if (nums[midIndex] === target) {
-    return midIndex;
-  }
+  if (nums[midIndex] === target) return midIndex;
 
   if (nums[midIndex] > target) {
     return searchInsert(nums.slice(0, midIndex), target, startIndex);
