b20c2125af7b4f38b0eb1e4a08940a71df3f5fc4
juyeonH <32149561+jy7123943@users.noreply.github.com>
2022-01-16 11:49:10 +0900

    Time: 120 ms (15.45%), Space: 39.8 MB (86.99%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.js
+++ 419-battleships-in-a-board/419-battleships-in-a-board.js
@@ -2,22 +2,15 @@
  * @param {character[][]} board
  * @return {number}
  */
-const searchDownRightX = function(board, x, y) {
-  if (board[x]?.[y] !== 'X') return;
-
-  board[x][y] = '.';
-
-  searchDownRightX(board, x, y + 1);
-  searchDownRightX(board, x + 1, y);
-};
 var countBattleships = function(board) {
   let count = 0;
 
   for (let x = 0; x < board.length; x++) {
     for (let y = 0; y < board[x].length; y++) {
       if (board[x][y] === 'X') {
+        if (board[x][y + 1] === 'X') continue;
+        if (board[x + 1]?.[y] === 'X') continue;
         count++;
-        searchDownRightX(board, x, y);
       }
     }
   }
