570d82c36b053d91c987ba78e7d469931066487e
juyeonH <32149561+jy7123943@users.noreply.github.com>
2022-01-23 16:11:52 +0900

    Time: 590 ms (5.93%), Space: 47.9 MB (6.67%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.js
+++ 1314-matrix-block-sum/1314-matrix-block-sum.js
@@ -4,16 +4,28 @@
  * @return {number[][]}
  */
 var matrixBlockSum = function(mat, k) {
+  const memoSum = {};
   const sumNumbers = (numbers) => numbers.reduce((sum, n) => sum += n, 0);
 
-  const calculate = (rowMin, rowMax, colMin, colMax) => (
-    mat.slice(rowMin, rowMax + 1)
-      .reduce((sum, row) => sum += sumNumbers(row.slice(colMin, colMax + 1)), 0)
-  );
+  const calculate = (rowMin, rowMax, colMin, colMax) => {
+    const key = `${rowMin}${rowMax}${colMin}${colMax}`;
 
+    if (memoSum[key]) return memoSum[key];
+
+    memoSum[key] = mat.slice(rowMin, rowMax + 1).reduce((sum, row) => sum += sumNumbers(row.slice(colMin, colMax + 1)), 0);
+    return memoSum[key];
+  };
+
+  const maxRowIndex = mat.length - 1;
+  const maxColIndex = mat[0].length - 1;
   return mat.map((row, rowIndex) => (
     row.map((col, colIndex) => (
-      calculate(Math.max(rowIndex - k, 0), rowIndex + k, Math.max(colIndex - k, 0), colIndex + k)
+      calculate(
+        Math.max(rowIndex - k, 0),
+        Math.min(rowIndex + k, maxRowIndex),
+        Math.max(colIndex - k, 0),
+        Math.min(colIndex + k, maxColIndex),
+      )
     ))
   ));
 };
