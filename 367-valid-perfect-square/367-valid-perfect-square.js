8ad05f49a77832ab14002a30aeec73be58d12603
juyeonH <32149561+jy7123943@users.noreply.github.com>
2022-01-23 11:47:25 +0900

    Time: 117 ms (27.71%), Space: 38.5 MB (74.78%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.js
+++ 367-valid-perfect-square/367-valid-perfect-square.js
@@ -3,5 +3,23 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-  return (num ** 0.5) % 1 === 0;
+  if (num === 1) return true;
+
+  let left = 0;
+  let right = num;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    const squaredMid = mid * mid;
+
+    if (squaredMid === num) {
+      return true;
+    } else if (squaredMid < num) {
+      left++;
+    } else {
+      right = mid;
+    }
+  }
+
+  return false;
 };
