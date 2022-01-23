5bcbc89de582ff7274a5612d9d59c35175f4e3ce
juyeonH <32149561+jy7123943@users.noreply.github.com>
2022-01-23 16:05:10 +0900

    Time: 1003 ms (5.19%), Space: 45.3 MB (7.41%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.js
+++ 1314-matrix-block-sum/1314-matrix-block-sum.js
@@ -4,17 +4,16 @@
  * @return {number[][]}
  */
 var matrixBlockSum = function(mat, k) {
-  const sliceArray = (arr, min, max) => arr.slice(Math.max(0, min), max + 1);
   const sumNumbers = (numbers) => numbers.reduce((sum, n) => sum += n, 0);
 
   const calculate = (rowMin, rowMax, colMin, colMax) => (
-    sliceArray(mat, rowMin, rowMax)
-      .reduce((sum, row) => sum += sumNumbers(sliceArray(row, colMin, colMax)), 0)
+    mat.slice(rowMin, rowMax + 1)
+      .reduce((sum, row) => sum += sumNumbers(row.slice(colMin, colMax + 1)), 0)
   );
 
-  return mat.slice().map((row, rowIndex) => (
-    row.slice().map((col, colIndex) => (
-      calculate(rowIndex - k, rowIndex + k, colIndex - k, colIndex + k)
+  return mat.map((row, rowIndex) => (
+    row.map((col, colIndex) => (
+      calculate(Math.max(rowIndex - k, 0), rowIndex + k, Math.max(colIndex - k, 0), colIndex + k)
     ))
   ));
 };
