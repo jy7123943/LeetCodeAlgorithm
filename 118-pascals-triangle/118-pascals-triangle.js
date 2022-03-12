71b31103fa5d49539c8a8675d840e076439a9249
juyeonH <32149561+jy7123943@users.noreply.github.com>
2022-03-12 22:47:28 +0900

    Time: 76 ms (64.31%), Space: 42 MB (47.70%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -3,20 +3,23 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-  const result = [];
+  const result = [[1]];
 
-  for (let i = 0; i < numRows; i++) {
-    const temp = new Array(i + 1).fill(1);
-
-    if (i >= 2) {
-      const prev = result[i - 1];
-
-      for (let j = 0; j < Math.floor(prev.length / 2); j++) {
-        temp[j + 1] = prev[j] + prev[j + 1];
-        temp[temp.length - j - 2] = prev[j] + prev[j + 1];
-      }
+  for (let i = 1; i < numRows; i++) {
+    if (i === 1) {
+      result.push([1, 1]);
+      continue;
     }
 
+    const prev = result[i - 1];
+    const temp = [1];
+
+    for (let j = 0; j < Math.floor(prev.length / 2); j++) {
+      temp[j + 1] = prev[j] + prev[j + 1];
+      temp[prev.length - j - 1] = prev[j] + prev[j + 1];
+    }
+
+    temp.push(1);
     result.push(temp);  
   }
 
